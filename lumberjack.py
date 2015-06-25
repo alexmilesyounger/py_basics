a32e9340779dbe90cd0160260471d5065ce8ac34
Alex Miles Younger <alex@alexmilesyounger.com>
2015-06-25 08:04:55 -0400

    Update to use string formatting

--- lumberjack.py
+++ lumberjack.py
@@ -1,8 +1,8 @@
 name = input("What's your name? ")
 
 if name == "Alex":
-	print(name + " is a lumberjack and he/she's OK!")
+	print("{} is a lumberjack and he/she's OK!".format(name)) # update to use string formatting
 else:
-	print(name + " sleeps all night and " + name + " works all day!")
+	print("{} sleeps all night and {} works all day!".format(name, name)) # update to use string formatting
 
 
