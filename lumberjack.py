91d80c1ff28701887b131f73cee3023779ba62ff
Alex Miles Younger <alex@alexmilesyounger.com>
2015-06-25 08:02:44 -0400

    Basic if/else check

---
+++ lumberjack.py
@@ -0,0 +1,8 @@
+name = input("What's your name? ")
+
+if name == "Alex":
+	print(name + " is a lumberjack and he/she's OK!")
+else:
+	print(name + " sleeps all night and " + name + " works all day!")
+
+
